4a9917493c523874467c4c4d5895691d59799057
Admin <everon1988@mail.ru>
2021-03-14 02:05:14 +0300

    no message

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.use('/api/terrain', require('./src/routes/terrains'));
 app.use('/api/map-cell', require('./src/routes/mapCell.route'));
 
 if (process.env.NODE_ENV === 'production') {
-    //app.use('/', express.static(path.join(__dirname, 'client', 'dist')));
-    app.use('/', express.static(path.join(__dirname, 'client', 'src')));
+    app.use('/', express.static(path.join(__dirname, 'client', 'dist')));
+    //app.use('/', express.static(path.join(__dirname, 'client', 'src')));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
