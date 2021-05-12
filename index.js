b148013d365661778f7f963237cb5eb166b21704
a.staroverov <Everon1988@mail.ru>
2021-05-12 09:58:53 +0300

    no message

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 if (process.env.NODE_ENV !== 'production') {
     app.use((req, res, next) => {
         res.header(`Access-Control-Allow-Origin`, req.headers.origin); //адрес сервера, на котором запущен фронтенд.
-        res.header(`Access-Control-Allow-Methods`, `*`);
+        res.header(`Access-Control-Allow-Methods`, `GET, POST, OPTIONS, PUT, DELETE`);
         res.header('Access-Control-Allow-Credentials', 'true');
         res.header(`Access-Control-Allow-Headers`, `Origin, X-Requested-With, Content-Type, Accept, Set-Cookie`);
         next();
